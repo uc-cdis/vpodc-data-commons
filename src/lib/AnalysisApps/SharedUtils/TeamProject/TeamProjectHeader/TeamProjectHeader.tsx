9adcad129f007c958f616e4792d7765366114ac8
Shawn O'Connor <shawnoconnor@uchicago.edu>
2025-10-14 11:47:39 -0500

    enable api call for Teams

--- src/lib/AnalysisApps/SharedUtils/TeamProject/TeamProjectHeader/TeamProjectHeader.tsx
+++ src/lib/AnalysisApps/SharedUtils/TeamProject/TeamProjectHeader/TeamProjectHeader.tsx
@@ -4,9 +4,9 @@ import EditIcon from './Icons/EditIcon';
 import isEnterOrSpace from '../../AccessibilityUtils/IsEnterOrSpace';
 import TeamProjectModal from '../TeamProjectModal/TeamProjectModal';
 import IsCurrentTeamProjectValid from './IsCurrentTeamProjectValid';
-//import { TeamProjectsEndpoint } from '../../Endpoints';
+import { TeamProjectsEndpoint } from '../../Endpoints';
 import { Loader } from '@mantine/core';
-//import useSWR from 'swr';
+import useSWR from 'swr';
 
 const runningApplicationClientSide = typeof window !== 'undefined';
 
@@ -46,22 +46,9 @@ const TeamProjectHeader: React.FC<TeamProjectHeaderProps> = ({
   }, [redirect]);
 
   // SWR CODE
-  /*const { data, error, isLoading } = useSWR(TeamProjectsEndpoint, (...args) =>
+  const { data, error, isLoading } = useSWR(TeamProjectsEndpoint, (...args) =>
     fetch(...args).then((res) => res.json()),
-  );*/
-  // TODO: Replace with actual API call when available
-  const data = {
-    teams: [
-        {
-          teamName: '/gwas_projects/project1',
-        },
-        {
-          teamName: '/gwas_projects/project2',
-        },
-      ],
-    };
-  const error = 'success';
-  const isLoading = false;
+  );
 
   let currentTeamProjectIsValid = false;
   if (data) {
