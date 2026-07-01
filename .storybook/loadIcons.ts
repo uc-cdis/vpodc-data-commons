bcab9a3abef32e3a4abc989c994623b2e55fb2cf
Shawn O'Connor <shawnoconnor@uchicago.edu>
2026-07-01 13:54:10 -0500

    fix lint errors

--- .storybook/loadIcons.ts
+++ .storybook/loadIcons.ts
@@ -1,17 +1,17 @@
 import { type RegisteredIcons } from '@gen3/frontend';
-// eslint-disable-next-line @typescript-eslint/no-var-requires
+// eslint-disable-next-line @typescript-eslint/no-require-imports
 const gen3Icons: RegisteredIcons = require(
   `../config/icons/gen3.json`,
 );
-// eslint-disable-next-line @typescript-eslint/no-var-requires
+// eslint-disable-next-line @typescript-eslint/no-require-imports
 const colorIcons: RegisteredIcons = require(
   `..//config/icons/color.json`,
 );
-// eslint-disable-next-line @typescript-eslint/no-var-requires
+// eslint-disable-next-line @typescript-eslint/no-require-imports
 const dictionaryIcons: RegisteredIcons = require(
   `../config/icons/dataDictionary.json`,
 );
-// eslint-disable-next-line @typescript-eslint/no-var-requires
+// eslint-disable-next-line @typescript-eslint/no-require-imports
 const workspaceIcons: RegisteredIcons = require(
   `../config/icons/workspace.json`,
 );
