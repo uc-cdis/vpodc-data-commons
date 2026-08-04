795e74f3dd1ad465d743f2264c1099301c696aed
Urvi Sheth <70549768+urvi-occ@users.noreply.github.com>
2026-08-04 08:41:30 -0500

    Chore/fix dataconnections (#138)
    * changes to fields and columns
    
    * changes to get color for Established tab
    
    * Update DataConnections.tsx
    
    ---------
    
    Co-authored-by: Urvi Sheth <urvisheth@Urvis-MacBook-Pro-2.local>

--- src/pages/DataConnections.tsx
+++ src/pages/DataConnections.tsx
@@ -177,6 +177,7 @@ const StatsPage = ({ headerProps, footerProps }: NavPageLayoutProps) => {
             <Table.Tr>
               <Table.Th>Source</Table.Th>
               <Table.Th>Cohort / Study</Table.Th>
+              <Table.Th>Data Type</Table.Th>
               <Table.Th>Data Description</Table.Th>
               <Table.Th>Connection Type</Table.Th>
               <Table.Th>Connection Status</Table.Th>
