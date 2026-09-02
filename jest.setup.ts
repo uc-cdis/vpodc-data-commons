7371474691a0b8906b7d04281c8099021816dda4
Shawn O'Connor <shawnoconnor@uchicago.edu>
2026-09-02 13:15:02 -0500

    test jest fix

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,7 +1,4 @@
 import '@testing-library/jest-dom';
 import { loadEnvConfig } from '@next/env';
-import module from 'module';
-if (typeof module.register === 'undefined' || process.env.JEST_WORKER_ID) {
-  module.register = () => {};
-}
+
 loadEnvConfig(__dirname, true, { info: () => null, error: console.error });
